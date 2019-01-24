cf68cad4b3dd33d1138e054e6f13fd49d7bf791f
tronze <0183535663@naver.com>
2019-01-24 10:27:26 +0900

    WebClasses: Initial commit

---
+++ web_classes.py
@@ -0,0 +1,33 @@
+class WebClasses:
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.classnames = []
+
+    def get_classnames(self):
+        classnames = self.classnames
+        return classnames
+
+    def add_classname(self, classname):
+        classnames = self.get_classnames()
+        if classname is not None and classname is not "" and self.classname_exist(classname) is False:
+            classnames.append(classname)
+
+    def delete_classname(self, classname):
+        pass
+
+    def classname_exist(self, classname):
+        classnames = self.get_classnames()
+        return classname in classnames
+
+    def create_classnames_property(self):
+        classnames = self.get_classnames()
+        prefix = "class=\""
+        postfix = "\""
+        class_properties = ""
+        for classname in classnames:
+            if class_properties is "":
+                class_properties = classname
+            else:
+                class_properties += " " + classname
+        return prefix + class_properties + postfix
