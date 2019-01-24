f88a8b4add26836780844b64777eac4560d6f34f
tronze <0183535663@naver.com>
2019-01-24 11:05:16 +0900

    WebElement > Table: Initial commit

---
+++ web_elements/table.py
@@ -0,0 +1,13 @@
+from .web_element import WebElement
+
+
+class TableElement(WebElement):
+
+    TABLE = 'table'
+    THEAD = 'thead'
+    TBODY = 'tbody'
+    TR = 'tr'
+    TD = 'td'
+
+    def __init__(self):
+        super().__init__(element_type=TableElement)
